358a11b4f3d5732fb7aba253c83d2a4bc9c7f2ee
Helena J Gomes <heju1958@outlook.com>
2022-09-29 15:33:58 -0300

    selecao de dados 2

--- selects.sql
+++ selects.sql
@@ -23,6 +23,18 @@ SELECT
 
 -- 2)
 
+SELECT 
+      pe.id
+    FROM
+      pedidos pe
+    JOIN produtos_pedidos pr_pe
+        ON
+      pe.id = pr_pe.pedidos_id
+    JOIN produtos pr 
+        ON 
+      pr_pe.produtos_id = pr.id
+      WHERE pr.nome = 'Fritas';
+
 -- 3)
 
 -- 4)
