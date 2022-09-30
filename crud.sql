e2484c566dc059ae957052fdb29decaf503bfb6d
Helena J Gomes <heju1958@outlook.com>
2022-09-30 14:43:58 -0300

    feat: crud - criação

--- crud.sql
+++ crud.sql
@@ -4,15 +4,27 @@
 
 -- 1)
 
-
+INSERT INTO clientes
+       (nome, lealdade)
+VALUES
+       ('Georgia', 0);
 
 -- 2)
 
-
+INSERT INTO pedidos 
+       (status, cliente_id)
+VALUES
+       ('Recebido', 6);
 
 -- 3)
 
-
+INSERT INTO produtos_pedidos
+       (pedidos_id, produtos_id)
+VALUES
+       (6, 1),
+ 	   (6, 2),
+       (6, 8),
+       (6, 8);
 
 -- Leitura
 
